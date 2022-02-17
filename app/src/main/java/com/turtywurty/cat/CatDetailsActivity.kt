8399d436b8cd6bb8006e3d33e359aca55cf9c8a0
brammie15 <Brammieverhulst@gmail.com>
2022-02-17 19:27:17 +0100

    add toolbar

--- app/src/main/java/com/turtywurty/cat/CatDetailsActivity.kt
+++ app/src/main/java/com/turtywurty/cat/CatDetailsActivity.kt
@@ -1,16 +1,28 @@
 package com.turtywurty.cat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toolbar
 import com.squareup.picasso.Picasso
 
 class CatDetailsActivity : AppCompatActivity() {
+    lateinit var toolbar : androidx.appcompat.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cat_details)
+        toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+        var backbtn = findViewById<ImageButton>(R.id.backbtn);
+        backbtn.setOnClickListener {
+            finish()
+        }
         val catUrl : String? = intent.getStringExtra("CatUrl")
         val catTags : String? = intent.getStringExtra("CatTags")
 
@@ -26,5 +38,10 @@ class CatDetailsActivity : AppCompatActivity() {
     override fun onBackPressed() {
         super.onBackPressed()
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.optionsmenu, menu)
+        return true
     }
 }
